3a7bad82590313cba551cacd3c7951200dcd3874
Fajar Hikmal <fajarhikmal214@gmail.com>
2026-06-17 12:00:32 +0700

    feat: add healthcheck API

---
+++ simple-net-core-api/Controllers/HealthcheckController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace simple_net_core_api.Controllers
+{
+    [Route("[controller]")]
+    public class HealthcheckController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                Status = "Healthy",
+                Version = "1.0.0"
+            });
+        }
+    }
+}
